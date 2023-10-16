09ac50cb971d4e678fe940e37d7feb7e48a297cd
Joel Rhea <joelw.rhea@gmail.com>
2023-10-15 20:04:09 -0400

    Created the code

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,9 @@
 -- Add your SQL here
+SELECT Movie_ID, Movie_Title, Rating
+FROM (
+    SELECT id as Movie_ID, name as Movie_Title, imdb_rating as Rating,
+           ROW_NUMBER() OVER (ORDER BY imdb_rating DESC) as rn
+    FROM movies
+    WHERE genre = 'Horror' AND release_year <= 1985
+) ranked
+WHERE rn <= 3;
